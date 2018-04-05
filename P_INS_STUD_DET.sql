39938422b7a5e2d6e293d035d2cf55c8a7c5354f
ankitbiswas15 <38063631+ankitbiswas15@users.noreply.github.com>
2018-04-05 12:01:21 +0530

    Add files via upload

---
+++ P_INS_STUD_DET.sql
@@ -0,0 +1,18 @@
+-- Unable to render PROCEDURE DDL for object ANKIT.P_INS_STUD_DET with DBMS_METADATA attempting internal generator.
+CREATE PROCEDURE P_INS_STUD_DET 
+is
+v_conf varchar2(5);
+begin
+DBMS_OUTPUT.PUT_LINE('Do you wish to continue? press Y for yes or N for no.');
+v_conf := y;
+if upper(v_conf) = 'Y'
+then
+v_sql := 'insert into student(studentid,firstname, lastname, date_of_birth,address,city,state,zipcode,telephone,fax,email) values(seq_studid.nextval,''';
+
+DBMS_OUTPUT.PUT_LINE('Please provide first name of student.');
+v_firstname := andy;
+v_sql := v_sql||v_firstname||''',';
+
+DBMS_OUTPUT.PUT_LINE(v_sql);
+end if; 
+end;
